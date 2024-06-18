911a67a6bc6ee1428748de7d55e44a6a975e8585
betfy <betfy.me@gmail.com>
2024-06-18 15:15:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,113 @@
 document.addEventListener("DOMContentLoaded", function () {
   const couponCode = "GANHA25";
 
-  if (window.innerWidth <= 768) {
-    function createButton() {
-      let button = document.createElement("button");
-      button.innerHTML = "<b>Resgatar Bônus</b>";
-      button.classList.add("col-6", "btn", "btn-primary");
-      button.style.position = "absolute";
-      button.style.bottom = "20px";
-      button.style.left = "50%";
-      button.style.transform = "translateX(-50%)";
-      document.querySelector(".modal-content").appendChild(button);
+  function createButton() {
+    let button = document.createElement("button");
+    button.innerHTML = "<b>Resgatar Bônus</b>";
+    button.classList.add("col-6", "btn", "btn-primary");
+    button.style.position = "absolute";
+    button.style.bottom = "20px";
+    button.style.left = "50%";
+    button.style.transform = "translateX(-50%)";
+    document.querySelector(".modal-content").appendChild(button);
 
-      button.addEventListener("click", function () {
-        let closeButton = document.querySelector(".modal-content a");
-        if (closeButton) closeButton.click();
-        observerPopUp.disconnect();
+    button.addEventListener("click", function () {
+      let closeButton = document.querySelector(".modal-content a");
+      if (closeButton) {
+        closeButton.click();
         setTimeout(function () {
-          let depositButton = document.querySelector(".d-block.mt-2 .btn-primary.btn-block");
+          let depositButton = document.querySelector(".NavBar_DepositButton__229mf");
           if (depositButton) {
-            observeDepositButton(depositButton);
             depositButton.click();
+            observeDepositForm();
           }
         }, 300);
-      });
-    }
-
-    function observeDepositButton(originalButton) {
-      const observerDepositButton = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-          const depositButton = document.querySelector(".d-block.mt-2 .btn-primary.btn-block");
-          const couponCheckbox = document.querySelector("#cupom");
-          const couponInput = document.querySelector(".d-block.mt-2 .form-control");
-          if (depositButton && depositButton === originalButton) {
-            observerDepositButton.disconnect();
-
-            const clonedButton = document.createElement("span");
-            clonedButton.innerHTML = depositButton.innerHTML;
-            clonedButton.className = depositButton.className;
-            depositButton.replaceWith(clonedButton);
-
-            clonedButton.addEventListener("click", function () {
-              if (!couponCheckbox.checked || couponInput.value !== couponCode) {
-                showAlertModal(() => {
-                  clonedButton.replaceWith(originalButton);
-                });
-              } else {
-                originalButton.click();
-              }
-            });
-          }
-        });
-      });
-      observerDepositButton.observe(document.body, { childList: true, subtree: true });
-    }
-
-    function showAlertModal(onClose) {
-      const alertModal = document.createElement("div");
-      alertModal.className = "alertify ajs-movable ajs-closable ajs-pinnable ajs-pulse";
-      alertModal.style.display = "block";
-      alertModal.innerHTML = `
-        <div class="ajs-dimmer"></div>
-        <div class="ajs-modal" tabindex="0">
-          <div class="ajs-dialog" tabindex="0">
-            <button class="ajs-reset"></button>
-            <div class="ajs-commands">
-              <button class="ajs-pin"></button>
-              <button class="ajs-maximize"></button>
-              <button class="ajs-close"></button>
-            </div>
-            <div class="ajs-header">Atenção</div>
-            <div class="ajs-body">
-              <div class="ajs-content">Utilize o cupom ${couponCode} para ganhar o bônus!</div>
-            </div>
-            <div class="ajs-footer">
-              <div class="ajs-auxiliary ajs-buttons"></div>
-              <div class="ajs-primary ajs-buttons">
-                <button class="ajs-button ajs-ok">OK</button>
-              </div>
-            </div>
-            <div class="ajs-handle"></div>
-            <button class="ajs-reset"></button>
-          </div>
-        </div>
-      `;
-      document.body.appendChild(alertModal);
-
-      function closeModal() {
-        alertModal.remove();
-        if (onClose) onClose();
       }
+    });
+  }
 
-      alertModal.querySelector(".ajs-ok").addEventListener("click", closeModal);
-      alertModal.querySelector(".ajs-close").addEventListener("click", closeModal);
-    }
-
-    const observerPopUp = new MutationObserver(function (mutations) {
+  function observeDepositForm() {
+    const observerDepositForm = new MutationObserver(function (mutations) {
       mutations.forEach(function (mutation) {
-        const popUp = document.querySelector(".modal-content");
-        if (popUp && !document.querySelector(".modal-content button.btn-primary")) {
-          createButton();
+        const depositButton = document.querySelector(".d-block.mt-2 .btn-primary.btn-block");
+        if (depositButton) {
+          observerDepositForm.disconnect();
+          replaceDepositButton();
         }
       });
     });
-    observerPopUp.observe(document.body, { childList: true, subtree: true });
+    observerDepositForm.observe(document.body, { childList: true, subtree: true });
   }
+
+  function replaceDepositButton() {
+    const originalButton = document.querySelector(".d-block.mt-2 .btn-primary.btn-block");
+    const clonedButton = document.createElement("span");
+    clonedButton.innerHTML = originalButton.innerHTML;
+    clonedButton.className = originalButton.className;
+    originalButton.classList.add("d-none");
+    originalButton.parentNode.insertBefore(clonedButton, originalButton.nextSibling);
+
+    clonedButton.addEventListener("click", function () {
+      const couponCheckbox = document.querySelector("#cupom");
+      const couponInput = document.querySelector(".d-block.mt-2 .form-control");
+      if (!couponCheckbox.checked || couponInput.value !== couponCode) {
+        showAlertModal(() => {
+          clonedButton.remove();
+          originalButton.classList.remove("d-none");
+        });
+      } else {
+        originalButton.click();
+      }
+    });
+  }
+
+  function showAlertModal(onClose) {
+    const alertModal = document.createElement("div");
+    alertModal.className = "alertify ajs-movable ajs-closable ajs-pinnable ajs-pulse";
+    alertModal.style.display = "block";
+    alertModal.innerHTML = `
+      <div class="ajs-dimmer"></div>
+      <div class="ajs-modal" tabindex="0">
+        <div class="ajs-dialog" tabindex="0">
+          <button class="ajs-reset"></button>
+          <div class="ajs-commands">
+            <button class="ajs-pin"></button>
+            <button class="ajs-maximize"></button>
+            <button class="ajs-close"></button>
+          </div>
+          <div class="ajs-header">Atenção</div>
+          <div class="ajs-body">
+            <div class="ajs-content">Utilize o cupom ${couponCode} para ganhar o bônus!</div>
+          </div>
+          <div class="ajs-footer">
+            <div class="ajs-auxiliary ajs-buttons"></div>
+            <div class="ajs-primary ajs-buttons">
+              <button class="ajs-button ajs-ok">OK</button>
+            </div>
+          </div>
+          <div class="ajs-handle"></div>
+          <button class="ajs-reset"></button>
+        </div>
+      </div>
+    `;
+    document.body.appendChild(alertModal);
+
+    function closeModal() {
+      alertModal.remove();
+      if (onClose) onClose();
+    }
+
+    alertModal.querySelector(".ajs-ok").addEventListener("click", closeModal);
+    alertModal.querySelector(".ajs-close").addEventListener("click", closeModal);
+  }
+
+  const observerPopUp = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      const popUp = document.querySelector(".modal-content");
+      if (popUp && !document.querySelector(".modal-content button.btn-primary")) {
+        createButton();
+      }
+    });
+  });
+  observerPopUp.observe(document.body, { childList: true, subtree: true });
 });
